df99c9521b2926c2f9d4c4eb46dc3773fef043aa
manuelangel0909 <archivosesup@hotmail.com>
2022-03-27 14:19:09 -0300

    con carrito

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-
-const port=process.env.PORT || 4001
-app.use(express.static("public"));
+const port=process.env.PORT || 4001;
+app.use (express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
 });
-
-app.listen(port, () => console.log("running on port 4001"));
+app.get("/productCar", (req, res) => {
+  res.sendFile(__dirname + "/views/productCar.html")
+});
+app.get("/login", function (req,res){
+  res.sendFile(__dirname + "/views/login.html")
+});
+app.listen(port, () => console.log("running on port 4001"))
